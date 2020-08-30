d28e204591f8dd9b4a7ee470f385f8208a9ae23c
linn0 <linn.shawn@outlook.com>
2020-08-30 22:11:29 +0800

    clean some codes

--- custom/.ccl-init.lisp
+++ custom/.ccl-init.lisp
@@ -8,6 +8,4 @@
 (mapc (lambda (dir) (push (namestring dir) asdf:*central-registry*))
   (uiop:subdirectories "home:app;interact;custom"))
 (mapc (lambda (system) (asdf:load-system system))
-  '("ext" "cffi" "alexandria" "osicat" "linedit" "log4cl" "cl-ppcre"))
-
-(linedit:install-repl :wrap-current t :eof-quits t)
+  '("ext" "cffi" "log4cl" "cl-ppcre"))
